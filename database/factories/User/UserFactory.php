eae732387910460f81132b67674a41be86078bf0
eerzho <eerzho@gmail.com>
2021-11-29 05:14:22 +0600

    used MakeUniqueValue class in UserFactory.php

--- database/factories/User/UserFactory.php
+++ database/factories/User/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories\User;
 
+use App\Components\UniqueValue\MakeUniqueValue;
 use App\Models\User\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
@@ -18,8 +19,8 @@ class UserFactory extends Factory
         return [
             'first_name' => $this->faker->firstName,
             'last_name'  => $this->faker->lastName,
-            'username'   => $this->faker->userName,
-            'email'      => $this->faker->email,
+            'username'   => MakeUniqueValue::getValue('users', 'username', $this->faker->userName),
+            'email'      => MakeUniqueValue::getValue('users', 'email', $this->faker->email),
             'password'   => Hash::make('password'),
         ];
     }
